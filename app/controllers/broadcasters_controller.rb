ef34e2cfae60dfe6f31ee3a069d03b40fd2f83e5
rjacoby <rjacoby@twitter.com>
2015-06-01 15:36:44 -0500

    working retrieval of only periscopes

--- app/controllers/broadcasters_controller.rb
+++ app/controllers/broadcasters_controller.rb
@@ -2,11 +2,30 @@
 class BroadcastersController < ApplicationController
   def show
     @twitter_id = params[:twitter_id]
-    @tweets = ApplicationController.twitter.user_timeline(@twitter_id)
-    @embeds = embeds(@tweets)
+    @tweets = ApplicationController.twitter.user_timeline(
+      @twitter_id,
+      count: 200,
+      include_rts: false,
+      exclude_replies: true
+    )
+    @embeds = embeds(tweets_with_periscopes(@tweets))
   end
 
   private
+
+  def tweets_with_periscopes(tweets)
+    periscope_tweets = tweets.select do |t|
+      has_periscope = false
+      t.urls.each do |u|
+        if u.expanded_url.to_s.match(/periscope/)
+          has_periscope = true
+          next
+        end
+      end
+      has_periscope
+    end
+    periscope_tweets
+  end
 
   def embeds(tweets)
     tweets.each_with_object({}) do |t, h|
