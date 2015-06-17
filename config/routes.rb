09b0c4d56a7a86a67816535d07b0fc0cbd28ca2a
rjacoby <rjacoby@twitter.com>
2015-06-17 10:14:59 -0500

    disable hashtag search (broken) to avoid scale issues

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   get '/user/:twitter_id/list', to: 'broadcasters#list'
   get '/user/:twitter_id/latest', to: 'broadcasters#latest'
 
-  get '/hashtag/:hashtag', to: 'broadcasters#hashtag_list'
-  get '/hashtag/:hashtag/list', to: 'broadcasters#hashtag_list'
-  get '/hashtag/:hashtag/latest', to: 'broadcasters#hashtag_latest'
+  # get '/hashtag/:hashtag', to: 'broadcasters#hashtag_list'
+  # get '/hashtag/:hashtag/list', to: 'broadcasters#hashtag_list'
+  # get '/hashtag/:hashtag/latest', to: 'broadcasters#hashtag_latest'
 
   root to: 'visitors#index'
 
