890c79ee86a22498ce107412c990ffb28818e01f
rjacoby <rjacoby@twitter.com>
2015-06-18 23:05:05 -0500

    DJ to lookup all whitelisted tweets

--- app/models/whitelist_user.rb
+++ app/models/whitelist_user.rb
@@ -7,4 +7,10 @@ class WhitelistUser < ActiveRecord::Base
   def self.channel_handles(channel)
     self.where('channel = ?', channel).pluck(:handle)
   end
+
+  def self.enqueue_whitelist_lookups
+    all_handles.each do |handle|
+      Lookup.delay.tweets_for(handle)
+    end
+  end
 end
